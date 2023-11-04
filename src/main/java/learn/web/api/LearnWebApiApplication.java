c61ee1117c78f477d0762f665a14f5b65d0e0832
PeterAdam <peter.adam.extern@adesso.de>
2023-11-04 15:58:21 +0100

    initial commit

---
+++ src/main/java/learn/web/api/LearnWebApiApplication.java
@@ -0,0 +1,13 @@
+package learn.web.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnWebApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnWebApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/learn/web/api/LearnWebApiApplicationTests.java
@@ -0,0 +1,13 @@
+package learn.web.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnWebApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
