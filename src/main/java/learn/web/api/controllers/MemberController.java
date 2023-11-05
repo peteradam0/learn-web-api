b94aea3b9b67d1577c4976b3af21f3e712fe62c0
PeterAdam <adampeter1998@yahoo.com>
2023-11-05 10:42:15 +0100

    chore: update endpoint for rest response

--- src/main/java/learn/web/api/controllers/MemberController.java
+++ src/main/java/learn/web/api/controllers/MemberController.java
@@ -3,13 +3,16 @@ package learn.web.api.controllers;
 import learn.web.api.facades.MemberFacade;
 import learn.web.api.facades.dtos.MemberData;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/v1")
 @CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true", allowedHeaders = "true")
 public class MemberController {
 
@@ -17,7 +20,7 @@ public class MemberController {
     private MemberFacade memberFacade;
 
     @GetMapping("/members")
-    public List<MemberData> handleGetMembers() {
-        return memberFacade.getMemberData();
+    public ResponseEntity<List<MemberData>> handleGetMembers() {
+        return ResponseEntity.ok(memberFacade.getMemberData());
     }
 }
