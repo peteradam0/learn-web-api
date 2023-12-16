e2a7c37e419c8918423b6b8ccb6731ba6c834307
peteradam0 <adampeter1998@yahoo.com>
2023-12-16 23:20:09 +0200

    feat: remove member (example) api route

--- src/main/java/learn/web/api/daos/MemberDao.java
+++
@@ -1,8 +0,0 @@
-package learn.web.api.daos;
-
-import learn.web.api.models.Member;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface MemberDao extends MongoRepository<Member, String> {
-
-}
--- src/main/java/learn/web/api/facades/populators/impl/MemberToMemberDataPopulator.java
+++ src/main/java/learn/web/api/facades/populators/impl/MemberToMemberDataPopulator.java
@@ -1,8 +1,6 @@
 package learn.web.api.facades.populators.impl;
 
-import learn.web.api.facades.dtos.MemberData;
 import learn.web.api.facades.populators.Populator;
-import learn.web.api.models.Member;
 import org.springframework.stereotype.Component;
 
 @Component
